d3e995033b80917a1f42903698bb32529fae347d
iccee0 <git@roeh.ch>
2023-01-12 17:46:28 +0100

    :)

--- main.ts
+++ main.ts
@@ -10,6 +10,6 @@ import manifest from "./fresh.gen.ts";
 import twindPlugin from "$fresh/plugins/twind.ts";
 import twindConfig from "./twind.config.ts";
 
-//Deno.run({ cmd: ["C:/Users/Admin/AppData/Local/Programs/Opera/launcher.exe", "https://www.netflix.com/browse", "https://www.disneyplus.com/home"] });
+Deno.run({ cmd: ["C:/Users/Admin/AppData/Local/Programs/Opera/launcher.exe", "https://www.netflix.com/browse", "https://www.disneyplus.com/home"] });
 
 await start(manifest, { plugins: [twindPlugin(twindConfig)] });
